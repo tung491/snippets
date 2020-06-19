74854bc44692d391a69aa1a2ccbca3f49b50df6a
tung491 <dosontung007@gmail.com>
2020-06-19 20:36:26 +0700

    Add docs string for flatten_list

--- flatten_list.py
+++ flatten_list.py
@@ -1,19 +1,25 @@
 """
-Flatten a n-D list (n > 2) using recursion
+Flatten a n-D list (n > 2) to 1-D list using recursion
 """
 
 
 def flatten(input_list: list) -> list:
+    """
+    Flatten child elements of elements to 1-D list
+    if this elements is list, tuple or set
+    :param input_list: list
+    :return: list
+    """
     if not input_list:
         return []
     head, *tail = input_list
-    is_iterable = any(isinstance(head, type_) for type_ in [list, tuple, set])
-    if is_iterable:
+    is_iterator = any(isinstance(head, type_) for type_ in [list, tuple, set])
+    if is_iterator:
         return flatten(head) + flatten(tail)
     return [head] + flatten(tail)
 
 
-def main():
+def main() -> None:
     print(flatten([1, [2, [3, 4]]]))
     print(flatten([1, (2, 3, {4, 5})]))
 
